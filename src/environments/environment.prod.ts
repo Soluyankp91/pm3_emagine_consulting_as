70855e0751f65b48b2af7bfd2b684fca1b534bd5
Sergii.l <Serhii.l@lvivity.com>
2022-06-19 11:39:49 +0300

    hot fix with apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     dev: false,
     qa: false,
-    apiUrl: 'https://pm3-prod-api.prodataconsult.com/',
+    apiUrl: 'https://pm3-prod-api.prodataconsult.com',
     sourcingUrl: 'https://web-sourcing-prod.prodataconsult.com',
     sharedAssets: "https://web-sourcing-prod.prodataconsult.com/api/shared-assets"
 };
