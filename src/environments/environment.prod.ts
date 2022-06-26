8779e4447371e66978e0e9a5233e0351c2277aba
Sergii.l <Serhii.l@lvivity.com>
2022-06-26 19:41:47 +0300

    udpate MSAL configs for devel and prod as well

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
     apiUrl: 'https://pm3-prod-api.prodataconsult.com',
     sourcingUrl: 'https://web-sourcing-prod.prodataconsult.com',
     sharedAssets: "https://web-sourcing-prod.prodataconsult.com/api/shared-assets",
-    msalClientId: '54e44fbe-ca87-45be-9344-9a3bb6dd0dca',
-    msalAuthorityUrl: 'https://login.microsoftonline.com/0749517d-d788-4fc5-b761-0cb1a1112694/',
-    msalInterceptorConfigUrl: 'api://5f63a91e-8bfd-40ea-b562-3dad54244ff7/access_as_user'
+    msalClientId: '6651cfbb-f282-4700-a1cf-f924304b0871',
+    msalAuthorityUrl: 'https://login.microsoftonline.com/f5df7d60-53fa-47bc-b519-6f2681e92dfd/',
+    msalInterceptorConfigUrl: 'api://9b7c3538-67bf-4e21-88ef-cb512296be90/access_as_user'
 };
