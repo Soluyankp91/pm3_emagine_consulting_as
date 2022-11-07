3fcaa23e09712aad883e67c4f8482e5fd5200ac6
Sergii.l <Serhii.l@lvivity.com>
2022-11-07 23:24:08 +0200

    validation hot fix after merge, UI fixes

--- src/app/workflow/workflow-sales/workflow-sales.model.ts
+++ src/app/workflow/workflow-sales/workflow-sales.model.ts
@@ -109,9 +109,9 @@ export class WorkflowSalesClientDataForm extends FormGroup {
             clientRateAndInvoicing: new FormControl(null),
             clientPrice: new FormControl(null, Validators.required),
             clientCurrency: new FormControl(null, Validators.required),
-            rateUnitTypeId: new FormControl(null, Validators.required),
+            rateUnitTypeId: new FormControl(null),
             clientInvoiceCurrency: new FormControl(null, Validators.required),
-            clientInvoiceFrequency: new FormControl(null, Validators.required),
+            clientInvoiceFrequency: new FormControl(null),
             clientInvoiceTime: new FormControl(null),
             clientInvoicingDate: new FormControl(null),
 
