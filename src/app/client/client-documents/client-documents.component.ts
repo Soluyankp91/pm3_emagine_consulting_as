9f9db1e6e1b65917066e2c91e23d3352ea4472e8
Andrii Yanyshyn <andrii.y@lvivity.com>
2022-11-30 21:25:10 +0200

    removed duplicated function call

--- src/app/client/client-documents/client-documents.component.ts
+++ src/app/client/client-documents/client-documents.component.ts
@@ -138,7 +138,6 @@ export class ClientDocumentsComponent extends AppComponentBase implements OnInit
             .pipe(finalize(() => {}))
             .subscribe(result => {
                 this.generalFileTypes = result;
-                this.getGeneralDocuments();
             });
     }
 
