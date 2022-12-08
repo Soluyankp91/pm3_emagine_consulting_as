5ab5d3ca4367238f9660cbf8e03819515e97c37a
Sergii.l <Serhii.l@lvivity.com>
2022-12-08 17:38:45 +0200

    progress bar on sync

--- src/app/shared/common/errors/error-dialog.service.ts
+++ src/app/shared/common/errors/error-dialog.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
 
 @Injectable()
 export class ErrorDialogService {
     private opened = false;
 
-    constructor(private dialog: MatDialog) { }
+    constructor(
+        private dialog: MatDialog,
+        private _spinnerService: NgxSpinnerService
+        ) { }
 
     openDialog(message: string, header: string, status?: number): void {
 
         if (!this.opened) {
+            this._spinnerService.hide();
             this.opened = true;
             const dialogRef = this.dialog.open(ErrorDialogComponent, {
                 data: { message, header, status },
