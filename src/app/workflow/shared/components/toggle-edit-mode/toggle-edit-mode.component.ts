83edf92f57f2a3551b247421241119abb91411a2
Sergii.l <Serhii.l@lvivity.com>
2023-01-19 14:02:14 +0200

    component for toggle edit mode

---
+++ src/app/workflow/shared/components/toggle-edit-mode/toggle-edit-mode.component.ts
@@ -0,0 +1,18 @@
+import { EventEmitter } from '@angular/core';
+import { Output } from '@angular/core';
+import { Input } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'toggle-edit-mode',
+	templateUrl: './toggle-edit-mode.component.html',
+	styleUrls: ['./toggle-edit-mode.component.scss'],
+})
+export class ToggleEditModeComponent {
+    @Input() canToggleEditMode: boolean;
+    @Output() editModeToggled = new EventEmitter<any>();
+	constructor() {}
+    toggleEditMode() {
+        this.editModeToggled.emit();
+	}
+}
