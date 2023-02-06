1ca4a8885d068d1b14fe14c837e221971b3860d6
sasha soluyan <Oleksandr_Soluian@epam.com>
2023-02-06 15:48:30 +0200

    update editor

--- src/app/contracts/master-templates/template-editor/editor/components/richedit/richedit.component.ts
+++ src/app/contracts/master-templates/template-editor/editor/components/richedit/richedit.component.ts
@@ -1,63 +1,221 @@
 import {
-  create, createOptions, HomeTabItemId, Options,
-  RibbonItem, RibbonTab, RibbonTabType, RichEdit
-} from 'devexpress-richedit';
+	AfterViewInit,
+	ChangeDetectionStrategy,
+	Component,
+	ElementRef,
+	Input,
+	OnDestroy,
+	SkipSelf,
+	ViewChild,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
 
-import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
+import {
+	create,
+	createOptions,
+	FileTabItemId,
+	HomeTabCommandId,
+	HomeTabItemId,
+	MailMergeTabItemId,
+	Options,
+	RibbonButtonItem,
+	RibbonTab,
+	RibbonTabType,
+	RichEdit,
+} from 'devexpress-richedit';
+import { CharacterPropertiesApi } from 'devexpress-richedit/lib/model-api/character-properties';
+import { ParagraphPropertiesApi } from 'devexpress-richedit/lib/model-api/paragraph';
+import { RibbonButtonItemOptions } from 'devexpress-richedit/lib/client/public/ribbon/items/button';
+
 import { IMergeField } from '../../_api/merge-fields.service';
+import { RicheditService } from '../../services/richedit.service';
+import { TransformMergeFiels } from '../../helpers/transform-merge-fields.helper';
+import { InsertMergeFieldPopupComponent } from '../insert-merge-field-popup/insert-merge-field-popup.component';
 @Component({
-  standalone: true,
-  selector: 'app-richedit',
-  template: '<div class="editor"></div>',
-  styleUrls: ['./richedit.component.scss']
+	standalone: true,
+	selector: 'app-richedit',
+	template: `
+		<div class="editor" #editor></div>
+		<app-insert-merge-field-popup #mergePopup [fields]="mergeFields" (mergeField)="mergeSelectedField($event)">
+		</app-insert-merge-field-popup>`,
+	styleUrls: ['./richedit.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	imports: [CommonModule, InsertMergeFieldPopupComponent],
 })
 export class RicheditComponent implements AfterViewInit, OnDestroy {
-  private rich: RichEdit;
+	@Input() template: File | Blob | ArrayBuffer | string = '';
+	@Input() mergeFields: IMergeField;
 
-  @Input() template: File | Blob | ArrayBuffer | string = '';
-  @Input() mergeFields: IMergeField;
-  @Output() save = new EventEmitter();
+	@ViewChild('editor') editor: ElementRef;
+	@ViewChild('mergePopup') private mergeFieldPopup: InsertMergeFieldPopupComponent;
 
-  constructor(private element: ElementRef) { }
+	templateAsBase64$: BehaviorSubject<string> = this._richeditService.templateAsBase64$;
+	hasUnsavedChanges$: BehaviorSubject<boolean> = this._richeditService.hasUnsavedChanges$;
 
-  ribbonCustomization(options: Options) {
-    const homeTab = options.ribbon.getTab(RibbonTabType.Home);
-  
-    const findElem: RibbonItem = homeTab.getItem(HomeTabItemId.Find);
-    const replaceElem: RibbonItem = homeTab.getItem(HomeTabItemId.Replace);
-    homeTab.removeItem(findElem);
-    homeTab.removeItem(replaceElem);
+	private _rich: RichEdit;
 
-    // insert tab
-    const findTabId = 'FindTabId';
-    options.ribbon.insertTab(
-      new RibbonTab('Find', findTabId, 
-      //@ts-ignore
-      [findElem, replaceElem]), 2);
-    
-    options.mailMerge.dataSource = [
-      this.mergeFields
-    ]
-  }  
+	constructor(private _element: ElementRef, @SkipSelf() private _richeditService: RicheditService) {}
 
-  ngAfterViewInit(): void {
-    const options: Options = createOptions();
-    this.ribbonCustomization(options);
+	ngAfterViewInit(): void {
+		const options: Options = createOptions();
 
-    options.width = 'calc(100vw - 160px)';
-    options.height = 'calc(100vh - 120px)';
-    options.events.saving = (s, f) => {
-      this.save.emit(f.base64);
-    }
+		this.setDimensions(options);
+		this.setUnit(options);
+		this.ribbonCustomization(options);
+		this.createDocument(this._element.nativeElement.firstElementChild, options);
+		this.registerDocumentEvents();
+		this.registerCustomEvents(options);
+	}
 
-    this.rich = create(this.element.nativeElement.firstElementChild, options);
-    this.rich.openDocument(this.template, 'name', 4);
-  }
+	setDimensions(options: Options) {
+		options.width = 'calc(100vw - 160px)';
+		options.height = 'calc(100vh - 240px)';
+	}
 
-  ngOnDestroy() {
-    if (this.rich) {
-      this.rich.dispose();
-      this.rich = null;
-    }
-  }
+	setUnit(options: Options) {
+		options.unit = 1;
+	}
+
+	createDocument(element: HTMLDivElement, options: Options) {
+		this._rich = create(element, options);
+		this._rich.openDocument(this.template, 'emagine_doc', 4);
+	}
+
+	registerDocumentEvents() {
+		this._rich.events.documentChanged.addHandler(() => {
+			this.hasUnsavedChanges$.next(this._rich.hasUnsavedChanges);
+		});
+		
+		this._rich.setCommandEnabled('formatPainter', false);
+		this._rich.events.selectionChanged.addHandler((a, b) => {
+			if (a.selection.intervals[0].length) {
+				a.setCommandEnabled('formatPainter', true);
+			} else {
+				a.setCommandEnabled('formatPainter', false);
+			}
+		});
+	}
+
+	ribbonCustomization(options: Options) {
+		const fileTab = options.ribbon.getTab(RibbonTabType.File);
+		const mergeTab = options.ribbon.getTab(RibbonTabType.MailMerge);
+		const homeTab = options.ribbon.getTab(RibbonTabType.Home);
+
+		const insertFieldBtnOpts: RibbonButtonItemOptions = { icon: 'dxre-icon-InsertDataField', showText: true };
+		const painterFormatBtnOpts: RibbonButtonItemOptions = { icon: 'palette', showText: false };
+
+		fileTab.insertItem(new RibbonButtonItem('updateStyles', 'Update Styles'), 5);
+		mergeTab.insertItem(new RibbonButtonItem('mergeField', 'Insert Merge Field', insertFieldBtnOpts), 2);
+		homeTab.insertItem(new RibbonButtonItem('formatPainter', 'Format Painter', painterFormatBtnOpts), 3)
+		
+		mergeTab.removeItem(MailMergeTabItemId.ShowInsertMergeFieldDialog);
+		fileTab.removeItem(FileTabItemId.ExportDocument);
+		homeTab.removeItem(HomeTabItemId.Paste);
+
+		this.insertCompareTab(options);
+
+		// merge fields
+		options.mailMerge.dataSource = [this.mergeFields];
+	}
+
+	insertCompareTab(options: Options) {
+		const selectBtnId = 'selectBtn';
+		const selectBtn = new RibbonButtonItem(selectBtnId, 'Select Document', {
+			showText: true,
+			beginGroup: true,
+			icon: 'home',
+		});
+
+		const compareTabId = 'CompareTabID';
+		options.ribbon.insertTab(new RibbonTab('Compare', compareTabId, [selectBtn]));
+	}
+
+	registerCustomEvents(options: Options) {
+		this._rich.events.customCommandExecuted.addHandler((s, e) => {
+			switch (e.commandName) {
+				case 'updateStyles':
+					this.updateFontsToDefault();
+					this.transformFiledsIntoMergeFields();
+					break;
+				case 'mergeField':
+					this._showMergeFieldModal();
+					break;
+				case 'formatPainter':
+					this.formatPainter();
+					break;
+			}
+		});
+	}
+
+	transformFiledsIntoMergeFields() {
+		TransformMergeFiels.updateMergeFields(this._rich);
+	}
+
+	updateFontsToDefault() {
+		this._rich.beginUpdate();
+		this._rich.selection.selectAll();
+		this._rich.executeCommand(HomeTabCommandId.ChangeFontName, 'Arial');
+		this._rich.endUpdate();
+	}
+
+	formatPainter() {
+		this.editor.nativeElement.classList.add('painter-format');
+		this._rich.setCommandEnabled('formatPainter', false);
+
+		let charProperties: CharacterPropertiesApi;
+		let prgphProperties: ParagraphPropertiesApi;
+		
+		const handler = (rich: RichEdit, e) => {
+			let interval = this._rich.selection.intervals[0];
+
+			rich.beginUpdate();
+			rich.document.setCharacterProperties(interval, charProperties);
+			rich.document.setParagraphProperties(interval, prgphProperties);
+			this._rich.setCommandEnabled('formatPainter', true);
+			rich.endUpdate();
+
+			this._rich.events.pointerUp.removeHandler(handler);
+			this._rich.events.selectionChanged.removeHandler(sHandler);
+			this.editor.nativeElement.classList.remove('painter-format');
+		}
+
+		const interval = this._rich.selection.intervals[0];
+		const sHandler = (rich: RichEdit, e) => {
+			charProperties = rich.document.getCharacterProperties(interval);
+			prgphProperties = rich.document.getParagraphProperties(interval);
+		}
+
+		if (!interval.length) {
+			this._rich.events.pointerUp.removeHandler(handler);
+			this._rich.events.selectionChanged.removeHandler(sHandler);
+			return;
+		}
+		
+		this._rich.events.selectionChanged.addHandler(sHandler);
+		this._rich.events.pointerUp.addHandler(handler);
+		
+	}
+
+	private _showMergeFieldModal() {
+		this.mergeFieldPopup.showPopup();
+	}
+
+	mergeSelectedField(field: string) {
+		const position = this._rich.selection.active;
+		this._rich.selection.activeSubDocument.fields.createMergeField(position, field);
+	}
+
+	ngOnDestroy() {
+		if (this._rich) {
+			this._rich.dispose();
+			this._rich = null;
+		}
+	}
+
+	public setTemplateAsBase64() {
+		this._rich.exportToBase64((s) => {
+			this.templateAsBase64$.next(s);
+		});
+	}
 }
--- src/app/contracts/master-templates/template-editor/editor/editor.component.ts
+++ src/app/contracts/master-templates/template-editor/editor/editor.component.ts
@@ -1,58 +1,98 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { tap } from 'rxjs/operators';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { filter, take, tap } from 'rxjs/operators';
+import { BehaviorSubject, combineLatest, Subject } from 'rxjs';
 
-// Project
+// Project Specific
 import { EditorService } from './_api/editor.service';
 import { RicheditComponent } from './components/richedit/richedit.component';
-import { ActivatedRoute } from '@angular/router';
 import { MergeFieldsService } from './_api/merge-fields.service';
+import { RicheditService } from './services/richedit.service';
 
 @Component({
-	standalone: true,
-	selector: 'app-editor',
-	templateUrl: './editor.component.html',
-	styleUrls: ['./editor.component.scss'],
-	imports: [
-		CommonModule,
+  standalone: true,
+  selector: 'app-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.scss'],
+  imports: [
+    CommonModule,
 
-		// components
-		RicheditComponent,
-	],
-	providers: [EditorService, MergeFieldsService],
+    // components
+    RicheditComponent
+  ],
+  providers: [
+    EditorService,
+    MergeFieldsService,
+    RicheditService
+  ]
 })
 export class EditorComponent implements OnInit, OnDestroy {
-	_destroy$ = new Subject();
+  _destroy$ = new Subject();
+  template$ = new BehaviorSubject<File | Blob | ArrayBuffer | string>(null);
+  mergeFields$ = this._mergeFieldsService.getMergeFields(this._route.snapshot.params.id);
+  
+  docReady$ = combineLatest([this.template$, this.mergeFields$]).pipe(
+    filter(res => !!res[0] && !!res[1])
+  );
 
-	template$ = new BehaviorSubject<File | Blob | ArrayBuffer | string>(null);
-	mergeFields$ = this._mergeFieldsService.getMergeFields(this._route.snapshot.params.id);
+  isLoading$ = new Subject();
+  hasUnsavedChanges$ = this._richeditService.hasUnsavedChanges$;
 
-	constructor(
-		private _editorService: EditorService,
-		private _mergeFieldsService: MergeFieldsService,
-		private _route: ActivatedRoute
-	) {}
+  @ViewChild(RicheditComponent) richEdit: RicheditComponent;
 
-	ngOnInit(): void {
-		const templateId = this._route.snapshot.params.id;
-		this._editorService
-			.getTemplate(templateId)
-			.pipe(tap((template) => this.template$.next(template)))
-			.subscribe(
-				() => {},
-				() => {
-					this.template$.next(this._editorService.getTemplateMock());
-				}
-			);
-	}
+  constructor(
+    private _editorService: EditorService,
+    private _richeditService: RicheditService,
+    private _mergeFieldsService: MergeFieldsService,
+    private _route: ActivatedRoute
+  ) {
+  }
 
-	onSave(template: string) {
-		const templateId = this._route.snapshot.params.id;
-		this._editorService.upsertTemplate(templateId, { value: template }).subscribe();
-	}
+  ngOnInit(): void {
+    const templateId = this._route.snapshot.params.id;
 
-	ngOnDestroy(): void {
-		this._destroy$.complete();
-	}
+    this._editorService.getTemplate(templateId).pipe(
+      tap(template => this.template$.next(template)),
+    ).subscribe(
+      () => {},
+      () => {
+        this.template$.next(this._editorService.getTemplateMock())
+      }
+    )
+  }
+
+  saveAsDraft() {
+    this.isLoading$.next(true);
+    this.richEdit.setTemplateAsBase64();
+    
+    this.richEdit.templateAsBase64$.pipe(
+      filter(res => !!res),
+      take(1)
+    ).subscribe(base64 => {
+      const templateId = this._route.snapshot.params.id;
+      this._editorService.saveAsDraftTemplate(templateId, { value: base64 }).subscribe(
+        () => this.isLoading$.next(false)
+      );
+    })   
+  }
+
+  saveAsComplete() {
+    this.isLoading$.next(true);
+    this.richEdit.setTemplateAsBase64();
+
+    this.richEdit.templateAsBase64$.pipe(
+      filter(res => !!res),
+      take(1)
+    ).subscribe(base64 => {
+      const templateId = this._route.snapshot.params.id;
+      this._editorService.comleteTemplate(templateId, { value: base64 }).subscribe(
+        () => this.isLoading$.next(false)
+      );
+    })   
+  }
+
+  ngOnDestroy(): void {
+    this._destroy$.complete();
+  }
 }
