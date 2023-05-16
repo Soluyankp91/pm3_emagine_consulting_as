e4f88381d4899fcceda8c693c51f6ee5372aeb70
sasha soluyan <orsn@emagine.org>
2023-05-16 17:52:39 +0300

    additional_merge_field

--- src/app/contracts/agreements/template-editor/settings/settings.component.ts
+++ src/app/contracts/agreements/template-editor/settings/settings.component.ts
@@ -317,11 +317,7 @@ export class SettingsComponent extends AppComponentBase implements OnInit, OnDes
 		}
 
 		toSend.attachments = this._createAttachments(this.agreementFormGroup.attachments.value);
-		toSend.signers = toSend.signers.map((signer: any) => {
-			const newObj = Object.assign({}, signer);
-			delete newObj.agreementSignerId;
-			return new AgreementDetailsSignerDto(newObj);
-		});
+		toSend.signers = toSend.signers.map((signer: any) => new AgreementDetailsSignerDto(signer));
 
 		if (!this.consultantPeriodId && !this.currentAgreementTemplate?.consultantPeriodId) {
 			toSend.clientPeriodId = this.clientPeriodId ?? this.currentAgreementTemplate?.clientPeriodId;
@@ -443,7 +439,7 @@ export class SettingsComponent extends AppComponentBase implements OnInit, OnDes
 						startDate: startDate,
 						isSignatureRequired: contractSigners.length ? true : false,
 						endDate: endDate,
-						signers: contractSigners,
+						signers: this.mapSigners(contractSigners),
 					});
 					this.showMainSpinner();
 					return this._apiServiceProxy2
@@ -496,7 +492,7 @@ export class SettingsComponent extends AppComponentBase implements OnInit, OnDes
 												isSignatureRequired: contractSigners.length
 													? true
 													: defaultTemplate.isSignatureRequired,
-												signers: contractSigners.length ? contractSigners : [],
+												signers: contractSigners.length ? this.mapSigners(contractSigners) : [],
 												attachments: defaultTemplate.attachments,
 												parentSelectedAttachmentIds: defaultTemplate.attachmentsFromParent
 													? defaultTemplate.attachmentsFromParent
@@ -525,7 +521,7 @@ export class SettingsComponent extends AppComponentBase implements OnInit, OnDes
 										startDate: startDate,
 										endDate: clientPeriodSales.noEndDate ? null : endDate,
 										isSignatureRequired: contractSigners.length ? true : false,
-										signers: contractSigners,
+										signers: this.mapSigners(contractSigners),
 									});
 									if (clientPeriodSales.noEndDate) {
 										this.noExpirationDateControl.setValue(true);
@@ -1076,7 +1072,7 @@ export class SettingsComponent extends AppComponentBase implements OnInit, OnDes
 							isSignatureRequired: this.agreementFormGroup.initialValue.signers.length
 								? true
 								: agreementTemplateDetailsDto.isSignatureRequired,
-							signers: this.agreementFormGroup.initialValue.signers,
+							signers: this.mapSigners(this.agreementFormGroup.initialValue.signers),
 							note: agreementTemplateDetailsDto.note,
 							parentSelectedAttachmentIds: agreementTemplateDetailsDto.attachmentsFromParent
 								? agreementTemplateDetailsDto.attachmentsFromParent
@@ -1171,7 +1167,10 @@ export class SettingsComponent extends AppComponentBase implements OnInit, OnDes
 							parentSelectedAttachmentIds: agreementDetailsDto.attachmentsFromParent
 								? agreementDetailsDto.attachmentsFromParent
 								: [],
-							signers: [...this.agreementFormGroup.initialValue.signers, ...agreementDetailsDto.signers],
+							signers: this.mapSigners([
+								...this.agreementFormGroup.initialValue.signers,
+								...agreementDetailsDto.signers,
+							]),
 							selectedInheritedFiles: [],
 						});
 					} else {
@@ -1194,7 +1193,7 @@ export class SettingsComponent extends AppComponentBase implements OnInit, OnDes
 							parentSelectedAttachmentIds: agreementDetailsDto.attachmentsFromParent
 								? agreementDetailsDto.attachmentsFromParent
 								: [],
-							signers: agreementDetailsDto.signers,
+							signers: this.mapSigners(agreementDetailsDto.signers),
 							selectedInheritedFiles: [],
 						});
 					}
@@ -1429,6 +1428,14 @@ export class SettingsComponent extends AppComponentBase implements OnInit, OnDes
 				message: `You\'ve selected “Receive from other party”. By doing so you are permanently discarding any previous document changes and disabling document editor.  Are you sure you want to proceed?`,
 				confirmButtonText: 'Discard',
 			},
+		});
+	}
+
+	private mapSigners(signers: AgreementDetailsSignerDto[]) {
+		return signers.map((signer: any) => {
+			const newObj = Object.assign({}, signer);
+			delete newObj.agreementSignerId;
+			return new AgreementDetailsSignerDto(newObj);
 		});
 	}
 }
