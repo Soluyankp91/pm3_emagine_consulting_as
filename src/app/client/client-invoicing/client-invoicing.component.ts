76227de23673b525641ba7611ac5fe7218a996c5
Sergii Laba <sila@emagine.org>
2023-05-16 17:32:05 +0300

    connect client invoicing settings

--- src/app/client/client-invoicing/client-invoicing.component.ts
+++ src/app/client/client-invoicing/client-invoicing.component.ts
@@ -1,28 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 import { UntypedFormControl } from '@angular/forms';
 import { DataShowOnInvoceOptions, EDataShownOnInvoce } from './client-invoicing.model';
-import { ClientsServiceProxy } from 'src/shared/service-proxies/service-proxies';
+import { ClientsServiceProxy, ConsultantShownOnClientInvoiceAs } from 'src/shared/service-proxies/service-proxies';
+import { AppComponentBase } from 'src/shared/app-component-base';
+import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-client-invoicing',
 	templateUrl: './client-invoicing.component.html',
 })
-export class ClientInvoicingComponent implements OnInit {
+export class ClientInvoicingComponent extends AppComponentBase implements OnInit {
 	invoicingDataShown = new UntypedFormControl(EDataShownOnInvoce.ConsultantName);
     dataShowOnInvoceOptions = DataShowOnInvoceOptions;
+    consultantShownOnClientInvoiceAs: { [key: string]: string };
+    clientId: number;
+    private _unsubscribe = new Subject();
 	constructor(
-        private readonly _clientService: ClientsServiceProxy
-    ) {}
+        injector: Injector,
+        private readonly _clientService: ClientsServiceProxy,
+        private _activatedRoute: ActivatedRoute
+    ) {
+        super(injector);
+    }
 
     ngOnInit(): void {
-        // this._clientService.dataShownOnInvoicingGET()
+        this.consultantShownOnClientInvoiceAs = this.getStaticEnumValue('consultantShownOnClientInvoiceAs');
+        this._activatedRoute.parent!.paramMap.pipe(
+            takeUntil(this._unsubscribe)
+        ).subscribe(params => {
+            this.clientId = +params.get('id')!;
+            // this.get
+        });
+        console.log(this.consultantShownOnClientInvoiceAs);
+        // this._clientService.invoicingSettings(this.clientId)
         //     .subscribe(result => {
         //         this.invoicingDataShown.setValue(EDataShownOnInvoce[result]);
         //     })
     }
 
-	onControlChange(value: EDataShownOnInvoce) {
-        // this._clientService.dataShownOnInvoicingPUT(value)
-        //     .subscribe();
+	onControlChange(value: ConsultantShownOnClientInvoiceAs) {
+        this._clientService.invoicingSettings(this.clientId, value)
+            .subscribe();
     }
 }
