cba5f4d0a5cc0c3053697e903f1dea47d11c578a
Sergii Laba <sila@emagine.org>
2023-05-24 20:43:17 +0300

    fix misspelling

--- src/app/contracts/shared/components/settings-tab/settings-tab.component.ts
+++ src/app/contracts/shared/components/settings-tab/settings-tab.component.ts
@@ -59,7 +59,7 @@ export class SettingsTabComponent implements OnInit {
 			if (isAgreement) {
 				this.tabs.push({
 					link: `${templateId}/archive`,
-					label: 'Acrhive',
+					label: 'Archive',
 					icon: 'archive-icon',
 				});
 			}
@@ -81,7 +81,7 @@ export class SettingsTabComponent implements OnInit {
 		if (isAgreement) {
 			this.tabs.push({
 				link: undefined,
-				label: 'Acrhive',
+				label: 'Archive',
 				disabled: true,
 				icon: 'archive-icon',
 			});
