cd3a44e9c6d63750ca8abd3e70425aa8d08f5434
Sergii Laba <sila@emagine.org>
2023-05-26 13:24:25 +0300

    cleanup

--- src/shared/app-component-base.ts
+++ src/shared/app-component-base.ts
@@ -6,7 +6,7 @@ import { TenantList } from "src/app/workflow/workflow-sales/workflow-sales.model
 import { ISelectableIdNameDto } from "src/app/workflow/workflow.model";
 import { environment } from "src/environments/environment";
 import { AppConsts } from "./AppConsts";
-import { AgreementSimpleListItemDto, API_BASE_URL, ContractDocumentInfoDto, CountryDto, EnumEntityTypeDto, IdNameDto, PurchaseOrderDto, WorkflowHistoryDto } from "./service-proxies/service-proxies";
+import { AgreementSimpleListItemDto, API_BASE_URL, ContractDocumentInfoDto, CountryDto, EnumEntityTypeDto, IdNameDto, PurchaseOrderDto } from "./service-proxies/service-proxies";
 import { EProfileImageLinkTypes } from "./AppEnums";
 import { InternalLookupService } from "src/app/shared/common/internal-lookup.service";
 import { MomentFormatPipe } from "./common/pipes/moment-format.pipe";
@@ -174,9 +174,6 @@ export abstract class AppComponentBase {
 
     trackByItem(index: number, item: any) {
         return item;
-    }
-    trackByOperationId(index: number, item: WorkflowHistoryDto) {
-        return item?.operationId;
     }
 
 	displayConsultantNameFn(option: any) {
