e6a98fb8b09df56f6ff181a7e5d054f36b87ee19
Sergii Laba <sila@emagine.org>
2023-06-08 14:01:03 +0300

    teamAndDivision display

--- src/app/workflow/shared/components/manager-team/manager-team.component.ts
+++ src/app/workflow/shared/components/manager-team/manager-team.component.ts
@@ -1,40 +1,62 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { InternalLookupService } from 'src/app/shared/common/internal-lookup.service';
 import { TeamsAndDivisionsNodeDto } from 'src/shared/service-proxies/service-proxies';
+import { InitialManagerTeam } from './manager-team.model';
 
 @Component({
 	selector: 'manager-team',
 	templateUrl: './manager-team.component.html',
 	styleUrls: ['./manager-team.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ManagerTeamComponent implements OnInit {
-    private _managerTeamId: number;
-    managerTeam: TeamsAndDivisionsNodeDto;
-    @Input()
+	private _managerTeamId: number;
+	managerTeam = InitialManagerTeam;
+	@Input()
 	set managerTeamId(value: number) {
-        // console.log(value);
 		this._managerTeamId = value;
-        // let hrdId = null;
-        // if (value === 53041) {
-        //     hrdId = 2;
-        // } else if (value === 53038) {
-        //     hrdId = 71;
-        // }
-        this._findTeamAndDivision(value);
+		this._findTeamAndDivision(value);
 	}
 	get managerTeamId(): number {
 		return this._managerTeamId;
 	}
+
 	teamsAndDivisionsNodes: TeamsAndDivisionsNodeDto[];
 	constructor(private readonly _internalLookupService: InternalLookupService) {}
 
 	ngOnInit(): void {
 		this.teamsAndDivisionsNodes = this._internalLookupService.getEnumValue('teamsAndDivisionsNodes');
-		console.log(this.teamsAndDivisionsNodes);
-		console.log(JSON.stringify(this.teamsAndDivisionsNodes));
 	}
 
-    private _findTeamAndDivision(id: number) {
-        this.managerTeam = this.teamsAndDivisionsNodes.find(x => x.id === id);
-    }
+	private _findTeamAndDivision(id: number) {
+		if (id === null || id === undefined) {
+			this.managerTeam = {
+				team: '',
+				tenant: '',
+				division: '',
+			};
+			return;
+		}
+		const node = this.teamsAndDivisionsNodes.find((x) => x.id === id);
+		let secondLvlNode: TeamsAndDivisionsNodeDto;
+		let thirdLvlNode: TeamsAndDivisionsNodeDto;
+		if (node.parentId === null) {
+			this.managerTeam.tenant = node.name;
+		} else {
+			secondLvlNode = this._findParentNode(node.parentId);
+			if (secondLvlNode?.parentId === null) {
+				this.managerTeam.tenant = node?.name;
+				this.managerTeam.division = secondLvlNode?.name;
+			} else {
+				thirdLvlNode = this._findParentNode(secondLvlNode?.parentId);
+				this.managerTeam.tenant = thirdLvlNode?.name;
+				this.managerTeam.division = secondLvlNode?.name;
+				this.managerTeam.team = node?.name;
+			}
+		}
+	}
+
+	private _findParentNode(parentId: number) {
+		return this.teamsAndDivisionsNodes?.find((x) => x.id === parentId);
+	}
 }
---
+++ src/app/workflow/shared/components/manager-team/manager-team.model.ts
@@ -0,0 +1,11 @@
+export interface IManagerTeamDisplay {
+    team?: string;
+    tenant?: string;
+    division?: string;
+}
+
+export const InitialManagerTeam: IManagerTeamDisplay = {
+    team: '',
+    tenant: '',
+    division: ''
+}
