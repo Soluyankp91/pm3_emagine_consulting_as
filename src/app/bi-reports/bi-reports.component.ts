b24af0588f86eabcb87af3fb3019783ea6d93005
Sergii Laba <sila@emagine.org>
2023-07-05 17:06:18 +0300

    add bi-report module add bi-report into emg-widget

---
+++ src/app/bi-reports/bi-reports.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bi-reports',
+  templateUrl: './bi-reports.component.html',
+  styleUrls: ['./bi-reports.component.scss']
+})
+export class BiReportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
