5fd857f0af589825530026dd52e8dc1aed416128
Sergii Laba <sila@emagine.org>
2023-07-05 17:56:57 +0300

    remove unnecessary files

--- src/app/bi-reports/bi-reports.component.ts
+++ src/app/bi-reports/bi-reports.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-bi-reports',
-  templateUrl: './bi-reports.component.html',
-  styleUrls: ['./bi-reports.component.scss']
+  template: `<iframe title="emagine Request Summary" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"
+  src="https://app.powerbi.com/reportEmbed?reportId=77ea36a8-69b3-4814-a0d0-bf46f6d6a159&autoAuth=true&ctid=f5df7d60-53fa-47bc-b519-6f2681e92dfd"
+  frameborder="0" allowFullScreen="true"></iframe>`,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BiReportsComponent implements OnInit {
-
+export class BiReportsComponent {
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
